28d118bf46b2bec787bd2b809c91870bcc685a52
Luca <luca.ambrosonee@gmail.com>
2023-12-30 14:18:52 +0100

    Number can now individually be switched

--- main.js
+++ main.js
@@ -15,8 +15,47 @@ function showAllNumbers() {
 }
 
 hideAllNumbers();
+
 let numbersUsed = [];
 let i = 0;
+
+var recentNumbersList = document.getElementById('recentNumbersList');
+function toggleNumber(number) {
+    if (document.getElementById("_" + number).classList.contains("hidden")) {
+        allNumbers.forEach(function (allNumbers) {
+            allNumbers.classList.remove('current');
+        });
+        document.getElementById("_" + number).classList.remove("hidden");
+        document.getElementById("_" + number).classList.add("visible");
+        document.getElementById("_" + number).classList.add("current");
+        numbersUsed.push(number);
+        var newRecentNumberElement = document.createElement('p');
+        newRecentNumberElement.id = "rand_" + number;
+        newRecentNumberElement.className = 'recentNumberText';
+        newRecentNumberElement.textContent = number;
+        var recentNumbersList = document.getElementById('recentNumbersList');
+        recentNumbersList.appendChild(newRecentNumberElement);
+        i++;
+        var missing = 90 - i;
+        document.getElementById('numberOfUsed').textContent = 'Numeri estratti: ' + i
+        document.getElementById('numberOfMissing').textContent = 'Numeri mancanti: ' + missing
+    } else if (document.getElementById("_" + number).classList.contains("visible")) {
+        var tabellaNum = document.getElementById("_" + number);
+        tabellaNum.classList.remove('current');
+        document.getElementById("_" + number).classList.remove("visible");
+        document.getElementById("_" + number).classList.add("hidden");
+        var indexToRemove = numbersUsed.indexOf(number);
+        if (indexToRemove !== -1) {
+            numbersUsed.splice(indexToRemove, 1);
+        }
+        i--;
+        var missing = 90 - i;
+        document.getElementById('numberOfUsed').textContent = 'Numeri estratti: ' + i
+        document.getElementById('numberOfMissing').textContent = 'Numeri mancanti: ' + missing
+        var removingElement = document.getElementById("rand_" + number);
+        removingElement.remove();
+    }
+}
 
 function getNewNumber() {
     allNumbers.forEach(function (allNumbers) {
@@ -40,12 +79,12 @@ function getNewNumber() {
     document.getElementById('_' + randomNewNumber).classList.add('visible');
     document.getElementById('_' + randomNewNumber).classList.add('current');
     var newRecentNumberElement = document.createElement('p');
+    newRecentNumberElement.id = "rand_" + randomNewNumber;
     newRecentNumberElement.className = 'recentNumberText';
     newRecentNumberElement.textContent = randomNewNumber;
-    var recentNumbersList = document.getElementById('recentNumbersList');
     recentNumbersList.appendChild(newRecentNumberElement);
     document.getElementById('numberOfUsed').textContent = 'Numeri estratti: ' + i
-    var missing = 90-i;
+    var missing = 90 - i;
     document.getElementById('numberOfMissing').textContent = 'Numeri mancanti: ' + missing
 }
 
@@ -64,9 +103,9 @@ function updateDateAndTime() {
     var monthName = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'][currentDate.getMonth()];
     var hours = currentDate.getHours();
     var minutes = currentDate.getMinutes();
-    var formattedDateTime = dayOfWeek + ' ' + dayOfMonth + ' ' + monthName + ' | ' + hours + ':' + (minutes < 10 ? '0' : '') + minutes;
+    var formattedDateTime = dayOfWeek + ' ' + dayOfMonth + ' ' + monthName + ' |     ' + hours + ':' + (minutes < 10 ? '0' : '') + minutes;
     document.getElementById('dateAndTime').textContent = formattedDateTime;
-  }
+}
 
 setInterval(function () {
     updateDateAndTime();
